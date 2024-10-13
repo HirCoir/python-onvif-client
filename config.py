04ed13f0766ee5dd6efea27f95b6ffc1df63d437
HirCoir <97632765+HirCoir@users.noreply.github.com>
2024-10-13 13:50:59 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Configuración del sistema
 import os
-
+VIDEOS_POR_PAGINA = 5
 DIRECTORIO_VIDEOS = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'videos')
 USUARIOS = {
     'admin': 'password123',
